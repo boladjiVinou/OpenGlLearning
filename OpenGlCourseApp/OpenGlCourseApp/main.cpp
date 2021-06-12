1807f044a5074bd5529f7e9c7b021edeef046f07
Boladji <danyvinou@gmail.com>
2021-06-12 17:17:13 -0400

    adding exercice 3

--- OpenGlCourseApp/OpenGlCourseApp/main.cpp
+++ OpenGlCourseApp/OpenGlCourseApp/main.cpp
@@ -2,10 +2,11 @@
 //#include "HelloTriangle_1.cpp"
 //#include "HelloRectangle.cpp"
 //#include "HelloTriangle_1.cpp"
-#include "HelloTriangle_2.cpp"
+//#include "HelloTriangle_2.cpp"
+#include "HelloTriangle_3.cpp"
 int main() 
 {
-	HelloTriangle_2 hello;
+	HelloTriangle_3 hello;
 	hello.run();
 	return 0;
 }
